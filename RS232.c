455583ce3cd5371b984c58d97424e18abf73d74a
yzhang51 <39026139+yzhang51@users.noreply.github.com>
2018-05-07 17:46:10 +0200

    Add files via upload

---
+++ RS232.c
@@ -0,0 +1,106 @@
+#include <in4073.h>
+#include "pc_terminal.c"
+#include <RS232.h>
+
+static volatile bool read = false;
+queue rx_queue;
+queue rx_buffer;
+Param p; queue q;
+
+void rs232_init(void)
+{
+	uart_init();
+	write = false;
+	read = false;
+}
+
+/*---------------------------------------------------------
+ *Data format: | SYNC(header) | StartByte | Data | CRC1 | CRC2 | StopByte | 
+  ---------------------------------------------------------
+ */
+
+queue rs232_read(Param p, int c)
+{
+	static int state = 0;
+	int i = 0;
+	int data = 0;
+	int check1 = check2 = 0;	//store CRC1
+	
+	init_queue(rx_queue);
+	init_queue(rx_buffer);
+	init_queue(rx_param);
+	p = {0};
+	
+	for(i=0; i<QUEUE_SIZE; i++)
+	{
+		rx_queue.Data[i] = rs232_getchar();
+		data = rx_queue.Data[i];
+		switch(state)
+		{
+			case 0:
+				if(data == StartByte)
+				{
+					init_queue(rx_queue);
+					init_queue(rx_buffer);
+					rx_buffer.last = DataLen;
+					read = true;
+					state = 1;
+				}
+				else	state = 0;
+				break;
+				
+			case 1:
+				if(data == CRC1)	
+				{
+					if(rx_buffer.count<rx_buffer.last)	state = 1;
+					else
+					{
+						state = 2;
+						check1 = data;
+					}
+				}
+				else
+				{
+					rx_buffer.Data[rx_buffer.count] = data;
+					rx_buffer.count++;
+				}
+				break;
+				
+			case 2:
+				if(data == CRC2)
+				{
+					check2 = data;
+					if(!check_CRC(check1, check2))		state = 3;
+					else	state = 0;
+				}
+				break;
+				
+			case 3:
+				if(data == StopByte)
+				{
+					if(p.count>=size)	p = {0};
+					store_data(p, rx_buffer, p.count);
+					p.count++;
+					read = false;
+					state = 0;
+				}
+				else	state = 0;
+				break;
+			
+			default: break;
+		}
+	}
+}
+
+bool check_CRC(int c1, int c2)
+
+void store_data(Param p, queue q, int i)
+{
+	p.Mode[i] = q.Data[0];
+	p.Lift[i] = 0xF0 & q.Data[1] + 0x0F & q.Data[2];
+	p.Roll[i] = 0xF0 & q.Data[3] + 0x0F & q.Data[4];
+	p.Pitch[i] = 0xF0 & q.Data[5] + 0x0F & q.Data[6];
+	p.Yaw[i] = 0xF0 & q.Data[7] + 0x0F & q.Data[8];
+	p.Keyboard[i] = q.Data[9];
+}
+
---
+++ RS232.h
@@ -0,0 +1,26 @@
+#ifndef _RS232_H_
+#define _RS232_H_
+
+#define StartByte 0xFF
+#define CRC1
+#define CRC2
+#define StopByte  0xFF
+#define DataLen 10
+#define size	5
+
+typedef struct{
+	uint8_t Mode[size];
+	uint16_t Lift[size];
+	uint16_t Roll[size];
+	uint16_t Pitch[size];
+	uint16_t Yaw[size];
+	uint8_t Keyboard[size];
+	int count;
+}Param;
+
+void rs232_init(void);
+queue rs232_read(Param p, int c);
+bool check_CRC(int c1, int c2);
+void store_data(Param p, queue q, int i);
+
+#endif
