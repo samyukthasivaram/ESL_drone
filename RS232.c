028889abbd371c4f346135fe5f76fc2c3bccda6d
yzhang51 <39026139+yzhang51@users.noreply.github.com>
2018-05-08 09:59:21 +0200

    Update RS232.c

--- RS232.c
+++ RS232.c
@@ -91,7 +91,7 @@ bool rs232_read(int p[DataSize])
 
 bool check_CRC(int c1, int c2)
 
-Param store_data(int p[DataSize], queue q)
+int store_data(int p[DataSize], queue q)
 {
 	for(i=0; i<DataSize; i++)
 		p[i] = q.Data[i];
