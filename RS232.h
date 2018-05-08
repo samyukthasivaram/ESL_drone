f98f48e45eee66138b4c678f2b3d285e40821e5e
yzhang51 <39026139+yzhang51@users.noreply.github.com>
2018-05-08 10:00:30 +0200

    Update RS232.h

--- RS232.h
+++ RS232.h
@@ -6,11 +6,10 @@
 #define CRC2
 #define StopByte  0xFF
 #define DataLen 10
-#define DataSize	10
 
 void rs232_init(void);
-bool rs232_read(int p[DataSize]);
+bool rs232_read(int p[DataLen]);
 bool check_CRC(int c1, int c2);
-int store_data(int p[DataSize], queue q);
+int store_data(int p[DataLen], queue q);
 
 #endif
