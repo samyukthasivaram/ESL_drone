5e6fda13212afce3872f400055ad9e6110df77a6
swarna06 <arrswarna@gmail.com>
2018-05-08 10:04:57 +0200

    Update RS232.c

--- RS232.c
+++ RS232.c
@@ -15,8 +15,30 @@ void rs232_init(void)
  *Data format: | SYNC(header) | StartByte | Data | CRC1 | CRC2 | StopByte | 
   ---------------------------------------------------------
  */
+unsigned cal_crc(int *ptr,int len) 
+{
+ uint16 crc;
+ unsigned int da;
+ unsigned int crc_ta[16]={ 
+ 0x0000,0x1021,0x2042,0x3063,0x4084,0x50a5,0x60c6,0x70e7,
+0x8108,0x9129,0xa14a,0xb16b,0xc18c,0xd1ad,0xe1ce,0xf1ef,
+ }
+ crc=0;
+ while(len--!=0) {
+ da=((uint)(crc/256))/16; 
+ crc<<=4; 
+ crc^=crc_ta[da^(ptr/16)]; 
 
-bool rs232_read(int p[DataLen])
+ da=((uint)(crc/256))/16; 
+ crc<<=4; 
+ crc^=crc_ta[da^(ptr&0x0f)]; 
+ ptr++;
+ }
+ return(crc);
+}
+
+
+bool rs232_read(int p[DataSize])
 {
 	static int state = 0;
 	int i = 0;
@@ -90,8 +112,11 @@ bool rs232_read(int p[DataLen])
 }
 
 bool check_CRC(int c1, int c2)
+{
+	
+}
 
-int store_data(int p[DataLen], queue q)
+Param store_data(int p[DataSize], queue q)
 {
 	for(i=0; i<DataSize; i++)
 		p[i] = q.Data[i];
