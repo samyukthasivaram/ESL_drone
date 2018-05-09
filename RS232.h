1c579d4902d344dfd87353c3446bae85a1b17561
yzhang51 <39026139+yzhang51@users.noreply.github.com>
2018-05-09 12:01:37 +0200

    Update RS232.h

--- RS232.h
+++ RS232.h
@@ -4,9 +4,23 @@
 #define StartByte 0xFF
 #define DataLen 10
 
+FILE *fp = fopen{"gnuplot/matlab", "a"};
+
+typedef struct{
+	uint8_t Mode;
+	int Joystick;
+	int Keyboard;
+	int Actuator;
+	int Sensor;
+	int Sensor_processing_chain;
+	int controller;
+}log;
+
 void rs232_init(void);
 bool rs232_read(int p[DataLen]);
 bool check_CRC(int c1, int c2, queue q);
 int store_data(int p[DataLen], queue q);
+void Data_logging(log l);
+unsigned cal_crc(unsigned int *ptr, unsigned int len)；
 
 #endif
