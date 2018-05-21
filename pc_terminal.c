94dd6ad385d5a6cd31d8b5d2ae72727b3a024feb
swarna06 <arrswarna@gmail.com>
2018-05-21 17:46:10 +0200

    Add files via upload

--- pc_terminal.c
+++ pc_terminal.c
@@ -21,9 +21,10 @@
 #include <termios.h>
 #include <sys/stat.h>
 #include "joystick.h"
+#include "crc.h"
 
 #define NAME_LENGTH 128
-
+#define POLY 0x8005
 int charsWaiting (int fd)
 {
   int count ;
@@ -36,45 +37,46 @@ int charsWaiting (int fd)
   return count ;
 }
 //CRC calculation without LUT
-/*uint16_t calc_crc(int frame[])
+int16_t calc_crc(int frame[])
 
 {
-    uint16_t crc,byte;
+    int16_t crc,byte;
     crc = 0;
     
-    for (int i=1; i< DLEN+1; i++)
+    for (int i=1; i< 11; i++)
     {
             byte=frame[i];
             crc ^=byte<<8;
 	    for(int k=8;k>0;k--)
-		{if(crc & 0x80)
+		{if(crc & 0x8000)
 			crc^=POLY;
 		 crc=crc<<1;
 		}
     
     }
     return crc;
-}*/
+}
+/*
 //CRC calculation with LUT
 unsigned cal_crc(unsigned int *ptr, unsigned int len) {
  unsigned int crc;
- unsigned int da;
+ unsigned char da;
  unsigned int crc_ta[16]={ 
  0x0000,0x1021,0x2042,0x3063,0x4084,0x50a5,0x60c6,0x70e7,
 0x8108,0x9129,0xa14a,0xb16b,0xc18c,0xd1ad,0xe1ce,0xf1ef,
  };
  crc=0;
  while(len--!=0) {
- da=((unsigned int)(crc/256))/16; 
+ da=((uint)(crc/256))/16; 
  crc<<=4; 
  crc^=crc_ta[da^(*ptr/16)]; 
- da=((unsigned int)(crc/256))/16; 
+ da=((uint)(crc/256))/16; 
  crc<<=4; 
  crc^=crc_ta[da^(*ptr&0x0f)];
  ptr++;
  }
  return(crc);
-}
+}*/
 
 /*------------------------------------------------------------
  * console I/O
@@ -143,7 +145,7 @@ int	term_getchar()
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
-#define JS_DEV	"/dev/input/js0"
+
 int serial_device = 0;
 int fd_RS232;
 
@@ -244,51 +246,49 @@ int 	rs232_putchar(char c)
 int main(int argc, char **argv)
 {
 	char	c;
-	int *tx_frame;
+	//int *tx_frame;
 	term_puts("\nTerminal program - Embedded Real-Time Systems\n");
 
 	term_initio();
 	rs232_open();
-        struct termios options;
 
 	term_puts("Type ^C to exit\n");
 
 	/* discard any incoming text
-	 
+	 */
 	while ((c = rs232_getchar_nb()) != -1)
 		fputc(c,stderr);
 
-
-
-	
-
 	/* send & receive
 	 */
-int fd;
-if ((fd = open(JS_DEV, O_RDONLY)) < 0) {
-		perror("jstest");
-		exit(1);
-	}
-	while(1)
+	for (;;)
 	{
-
 	int16_t roll=0, pitch=0, yaw=0,lift=0;
 	int abort;
+	//int8_t temp_1,temp_2;
 	int flag_mode = 0;
-	int mode = 0;
-	int start=0xFF; 
-	int c=0;
-	//struct termios options;
+	static int8_t mode = 0;
+	int8_t start=0xFF; 
+	uint8_t c=0;
+	struct termios options;
 	int count=0;
-	int keyboard=0xF0;
+	int fd;
+	int8_t keyboard=0xF0;
 	int	axis[6];
 	int	button[12];
-	int tx_buffer[14];
-	uint16_t crc=0x00;
+	int tx_buffer[13];
+	int16_t crc=0x0000;
 	struct js_event js;
-	
+	/* 
 	// input from joystick 
-	//non-blocking mode
+
+#define JS_DEV	"/dev/input/js0"
+	if ((fd = open(JS_DEV, O_RDONLY)) < 0) {
+		perror("joystick error");
+		exit(1);
+	}
+
+	// non-blocking mode
 	 
 	fcntl(fd, F_SETFL, O_NONBLOCK);
 		
@@ -297,8 +297,8 @@ if ((fd = open(JS_DEV, O_RDONLY)) < 0) {
 			while (read(fd, &js, sizeof(struct js_event)) == 
 		       			sizeof(struct js_event))  {
 
-			//register data
-			
+			// register data
+			 
 			// fprintf(stderr,".");
 			switch(js.type & ~JS_EVENT_INIT) {
 				case JS_EVENT_BUTTON:
@@ -315,9 +315,9 @@ if ((fd = open(JS_DEV, O_RDONLY)) < 0) {
 		}		
 	
 		if(button[0]==1)
-			 
-			break;
-				
+			{mode= 0x09;
+			flag_mode = 1;
+			}	
 		else 
 		{
 		pitch = axis[0];
@@ -325,35 +325,38 @@ if ((fd = open(JS_DEV, O_RDONLY)) < 0) {
 		yaw = axis[2];
 		lift = axis[3];
 		}
-		
-/*
+
+*/             
+		lift = 15000;
+		roll = 20000;
 		pitch = 32000;
-		roll = 32000;
-		yaw = 32000;
-		lift = 32000;*/
+		yaw = 10000;
+                //printf("%d|%d|%d|%d|\t\n",pitch,roll,yaw,lift);
+			
 		//keyboard press 
-				
-			tcgetattr (fileno (stdin), &options) ;
+		if(flag_mode!=1) // does not take input from the keyboard if aborted in the joystick
+		{		
+			/*tcgetattr (fileno (stdin), &options) ;
 			cfmakeraw (&options) ;	// Note - also affects output
 			tcsetattr (fileno (stdin), TCSANOW, &options) ;
 			count = charsWaiting (fileno (stdin)) ;
-			if(count==1)
-			{ 
-				c = getchar () ;
-			}
-			else if (count==3)
-			{
+			c=getchar();*/
+			
 				int temp[3];
 				for(int i=0; i<3; i++)
-				temp[i]= getchar ();
+				{temp[i]= getchar ();
+				//printf("temp[%d]=%d \t",i,temp[i]);
+				}
 				if(temp[0]== 0x1B && (temp[1])== 0x5B)
-				c = temp[2];
-			}	
-			else 
-				c = 0x00;
+					c = temp[2];
+				else 
+					c = temp[0];
+			        //printf("c=%d\n",c);
+				
 			switch (c)
 			{
 				case 0x30:
+
 					mode = 0;
 				break;
 				case 0x31:
@@ -426,10 +429,16 @@ if ((fd = open(JS_DEV, O_RDONLY)) < 0) {
 					keyboard = 0x0D;
 				break;
 				default:
+					
 					keyboard = 0xF0;
-			}								
-			
-			
+			}
+										
+		}
+		else
+		{
+			keyboard = 0xF0;
+		}	
+			//printf("mode =%d |KEYBOARD=%d \t",mode,keyboard);
 			// frame update
 			
 			tx_buffer[0]= start;
@@ -444,25 +453,40 @@ if ((fd = open(JS_DEV, O_RDONLY)) < 0) {
 			tx_buffer[9]= (int8_t)(yaw>>8);
 			tx_buffer[10]= keyboard;
 			
+			/*tx_buffer[0]= start;
+			tx_buffer[1]= 85;
+			tx_buffer[2]= 20;
+			tx_buffer[3]= 21;
+			tx_buffer[4]= 22;
+			tx_buffer[5]= 23;
+			tx_buffer[6]= 24;
+			tx_buffer[7]= 25;
+			tx_buffer[8]= 26;
+			tx_buffer[9]= 27;
+			tx_buffer[10]=28;*/
 			//update crc
-			//crc=calc_crc(tx_buffer);      //calculate crc without LUT
-			//crc=cal_crc(tx_buffer,10);      //with LUT
-			crc=0xffd2;
-			
-			tx_buffer[11]= (uint8_t)crc;
-			tx_buffer[12]=(uint8_t)(crc>>8);
+			crc=calc_crc(tx_buffer);      //calculate crc without LUT
+			//crc=cal_crc(&tx_buffer,10);      //with LUT
+			//crc=0xFFFF;
+			//printf("crc=%d\t",crc);
+			//temp_1= (uint8_t)crc;
+			//temp_2=(uint8_t)(crc>>8); 
+			tx_buffer[11]= (int8_t)crc;
+			tx_buffer[12]=(int8_t)(crc>>8);; 
+
+		
 	
 	for(int k=0; k<13; k++)
 	{
-			rs232_putchar(tx_buffer[k]);
-			printf("%d",tx_buffer[k]);
-			
+			//tx_buffer[k]=0xFF;
+			rs232_putchar(tx_buffer[k]);//usleep(10);
+			//printf("tx[%d]=%d | \t ",k,tx_buffer[k]);
 	}
 	
-		//if ((c = rs232_getchar_nb()) != -1)
-		//	term_putchar(c);
+		if ((c = rs232_getchar_nb()) != -1)
+			term_putchar(c);
 	
-	usleep(10000);
+	
 		
 	}
 
