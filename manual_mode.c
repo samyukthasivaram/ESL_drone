a26a3e38628fba6e957843f1cd48267bdbe047be
swarna06 <arrswarna@gmail.com>
2018-05-22 09:04:40 +0200

    Update manual_mode.c

--- manual_mode.c
+++ manual_mode.c
@@ -48,26 +48,23 @@ ae4 = squareroot(w4_sqr);
 
 void manual_mode_withoutsqrt()
 {
-int ae1 = 0;
-int ae2 = 0;
-int ae3 = 0;
-int ae4 = 0;
-ae1= -yaw - lift + pitch + pitch;
-ae2= -yaw - lift - pitch - pitch;
-ae3= yaw - lift - roll - roll;
-ae4= yaw - lift + roll + roll;
+
+ae[0]= -yaw - lift + pitch + pitch;
+ae[1]= -yaw - lift - pitch - pitch;
+ae[2]= yaw - lift - roll - roll;
+ae[3]= yaw - lift + roll + roll;
 
 
 
- if (ae1 < 0) ae1 = 0; 
- if (ae1 > 500) ae1 = 500;
- if (ae2 < 0) ae2 = 0; 
- if (ae2 > 500) ae2 = 500;
- if (ae3 < 0) ae3 = 0; 
- if (ae3 > 500) ae3 = 500;
- if (ae4 < 0) ae4 = 0; 
- if (ae4 > 500) ae4 = 500;
- 
+ if (ae[0] < 0) ae[0] = 0; 
+ if (ae[0] > 500) ae[0] = 500;
+ if (ae[1] < 0) ae[1] = 0; 
+ if (ae[1] > 500) ae[1] = 500;
+ if (ae[2] < 0) ae[2] = 0; 
+ if (ae[2] > 500) ae[2] = 500;
+ if (ae[3] < 0) ae[3] = 0; 
+ if (ae[3] > 500) ae[3] = 500;
+ printf("%d|%d|%d|%d|",ae[0],ae[1],ae[2],ae[3]);
 }
 
 void manual_mode_lookup()
